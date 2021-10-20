d1311a5f2d3c5f9cd71aa7607579a8713180bcb2
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2021-10-20 10:43:37 +0200

    Initial commit

---
+++ FACIT_Lucktext/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+string name = "";
+string favoriteSport = "";
+string petName = "";
+
+Console.WriteLine("Skriv in ditt namn:");
+name = Console.ReadLine();
+
+Console.WriteLine("Skriv in din favoritsport:");
+favoriteSport = Console.ReadLine();
+
+Console.WriteLine("Skriv in namnet på ditt husdjur:");
+petName = Console.ReadLine();
+
+//------------------------------------------------------------------------------
+
+Console.WriteLine($"Hördu {name}, jag har hört att du är jättedålig på {favoriteSport}.");
+Console.WriteLine($"Du är så dålig att till och med {petName} skrattar åt dig!");
+
+Console.WriteLine("Tryck på ENTER för att avsluta.");
+Console.ReadLine();
